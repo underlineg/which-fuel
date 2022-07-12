8ba27b4052389ca9a6a90ac04388364922ecc758
underlineg <batista.guilherme@gmail.com>
2022-07-12 16:35:34 -0300

    Main js app basic definition

---
+++ js/main.js
@@ -0,0 +1,42 @@
+export let init = function(){
+    let target = document.querySelector('.btn-calcular')
+    let alertTarget = document.querySelector('.alert-user')
+    let fuel1Value, fuel2Value, count = "";
+    let lim = 0.7;
+    let baseAlertHTML = "";
+    
+    target.addEventListener("click", (x)=> {
+        x.preventDefault()
+
+        fuel1Value = document.querySelector('#alchool').value
+        fuel2Value = document.querySelector('#gasoline').value
+
+        count = fuel1Value / fuel2Value
+
+        baseAlertHTML = "É melhor você abastecer com"
+        
+        if(count <= lim){
+            //usar alcool
+            baseAlertHTML += ` alcool`
+            baseAlertHTML += `<ul>
+                <li>Apesar do alcool gastar mais, ele é mais economico nesse momento</li>
+                <li>Diferença de preço de ${Math.round(count*100)/100} %</li>
+            </ul>`
+        }else{
+            //usar gasolina
+            baseAlertHTML += ` gasolina`
+            baseAlertHTML += `<ul>
+                <li>Apesar da gasolina ser mais cara, seu rendimento é maior, então é a opção mais economica nesse momento</li>
+                <li>Diferença de preço de ${Math.round(count*100)/100} %</li>
+            </ul>`
+        }
+
+        alertTarget.classList.add('open')
+        alertTarget.innerHTML = baseAlertHTML;
+        
+        
+    })
+}
+
+
+init();
