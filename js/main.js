c9f0df54f0a4eca7fe2977ded3a35d594dd2ff8d
underlineg <batista.guilherme@gmail.com>
2022-07-13 01:12:59 -0300

    Add component for input values
    Prevents user input 0 or none values in input

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import {dealInputValue} from './dealInputValue'
+
 export let init = function(){
     let target = document.querySelector('.btn-calcular')
     let alertTarget = document.querySelector('.alert-user')
@@ -9,33 +11,39 @@ export let init = function(){
     target.addEventListener("click", (x)=> {
         x.preventDefault()
 
-        fuel1Value = parseFloat(document.querySelector('#alchool').value.replace(',', ".").replace("R$",""));
-        fuel2Value = parseFloat(document.querySelector('#gasoline').value.replace(',', ".").replace("R$",""));
-        
-        count = fuel1Value / fuel2Value
+       dealInputValue()
+        .then( x => {
+            if( !x.state ) {
+                alert(x.message)
+                return false;
+            }
+            fuel1Value = parseFloat(document.querySelector('#alchool').value.replace(',', ".").replace("R$",""));
+            fuel2Value = parseFloat(document.querySelector('#gasoline').value.replace(',', ".").replace("R$",""));
+            
+            count = fuel1Value / fuel2Value
 
-        baseAlertHTML = ""
-        
-        if(count <= lim){
-            //usar alcool
-            baseAlertHTML += `alcool`
-            baseAlertHTML += `<ul>
-                <li>Apesar do alcool gastar mais, ele é mais economico nesse momento</li>
-                <li>Diferença de preço de ${Math.round(count*100)/100} %</li>
-            </ul>`
-        }else{
-            //usar gasolina
-            baseAlertHTML += `gasolina`
-            baseAlertHTML += `<ul>
-                <li>Apesar da gasolina ser mais cara, seu rendimento é maior, então é a opção mais economica nesse momento</li>
-                <li>Diferença de preço de ${Math.round(count*100)/100} %</li>
-            </ul>`
-        }
+            baseAlertHTML = ""
+            
+            if(count <= lim){
+                //usar alcool
+                baseAlertHTML += `alcool`
+                baseAlertHTML += `<ul>
+                    <li>Apesar do alcool gastar mais, ele é mais economico nesse momento</li>
+                    <li>Diferença de preço de ${Math.round(count*100)/100} %</li>
+                </ul>`
+            }else{
+                //usar gasolina
+                baseAlertHTML += `gasolina`
+                baseAlertHTML += `<ul>
+                    <li>Apesar da gasolina ser mais cara, seu rendimento é maior, então é a opção mais economica nesse momento</li>
+                    <li>Diferença de preço de ${Math.round(count*100)/100} %</li>
+                </ul>`
+            }
 
-        alertTarget.classList.add('open')
-        alertTarget.innerHTML = baseAlertHTML;
-        
-        
+            alertTarget.classList.add('open')
+            alertTarget.innerHTML = baseAlertHTML;
+            
+        })
     })
 }
 
